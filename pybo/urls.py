17169dc21f3edf861bed005ab8f54d60ab0bc127
HOME-2700 <metalhwal@gmail.com>
2021-12-23 20:44:54 +0900

    pybo: add question detail view

--- pybo/urls.py
+++ pybo/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from pybo import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('<int:question_id>/', views.detail),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -10,3 +10,11 @@ def index(request: HttpRequest) -> HttpResponse:
     question_list = Question.objects.order_by('-create_date')
     context = { 'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    """Print pybo detail
+    """
+    question = Question.objects.get(id=question_id)
+    context = { 'question': question }
+    return render(request, 'pybo/question_detail.html', context)
