bb4415b49d09b0b95fc01645bd17528b2fc77e9e
HOME-2700 <metalhwal@gmail.com>
2021-12-23 20:40:30 +0900

    pybo: index view : question list print

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
+from .models import Question
+
+
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("안녕")
+    """Print pybo list
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = { 'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
