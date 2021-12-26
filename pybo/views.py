70a2ae60f761d0f03c13db8df358fa7e43b71411
HOME-2700 <metalhwal@gmail.com>
2021-12-27 00:03:05 +0900

    pybo: 2-06 Add answer POST form and answer save method, display answers

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from django.views import generic
 
 from .models import Question
@@ -23,3 +24,17 @@ def detail(request: HttpRequest, question_id: int) -> HttpResponse:
 class IndexView(generic.ListView):
     def get_queryset(self):
         return Question.objects.order_by('-create_date')
+
+
+def answer_create(request: HttpRequest, question_id: int) -> HttpResponse:
+    """
+    pybo 답변 등록
+    """
+    print("wow")
+    question = get_object_or_404(Question, pk=question_id)
+    # foreign key 로 연결되어있기 때문에 다음과 같이 new Answer 생성 가능.
+    # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    # Alternatively, direct creation through Answer class
+    # answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    # answer.save()
+    return redirect('pybo:detail', question_id=question.id)
