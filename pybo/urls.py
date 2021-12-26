48cd623e9675540ca9db0ab400660be83279e435
HOME-2700 <metalhwal@gmail.com>
2021-12-26 23:45:25 +0900

    pybo: 2-05 URL and namespace

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from pybo import views
+from . import views
+
+app_name = 'pybo'
 
 urlpatterns = [
-    # path('', views.index),
-    path('', views.IndexView.as_view()),
-    path('<int:question_id>/', views.detail),
+    path('', views.index, name='index'),
+    # path('', views.IndexView.as_view()),
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
