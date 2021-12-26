a17e9b28beec883743cc42bdf89a3d06a9d91d00
HOME-2700 <metalhwal@gmail.com>
2021-12-27 00:54:58 +0900

    pybo: 2-10 Question register Form style `css` via Form metaclass

--- pybo/forms.py
+++ pybo/forms.py
@@ -5,3 +5,11 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question # model to use
         fields = ['subject', 'content'] # model attributes for using in QuestionForm
+        widgets = {
+            'subject': forms.TextInput(attrs={ 'class': 'form-control' }),
+            'content': forms.Textarea(attrs={ 'class': 'form-control', 'rows': 10 }),
+        }
+        labels = {
+            'subject': '제목',
+            'content': '내용',
+        }
