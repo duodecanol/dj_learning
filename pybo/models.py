19889bfcb281a9d7d8eaf876182a45a16025455c
HOME-2700 <metalhwal@gmail.com>
2021-12-27 18:16:52 +0900

    pybo: 3-12 글 추천 기능 [1] Question/Answer 추천인 필드 추가

--- pybo/models.py
+++ pybo/models.py
@@ -9,11 +9,12 @@ class Question(models.Model):
         `content` longtext NOT NULL,
         `create_date` datetime(6) NOT NULL);
     """
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question') # 추천인
     # null=True는 데이터베이스에서 modify_date 칼럼에 null을 허용한다는 의미이며,
     # blank=True는 form.is_valid()를 통한 입력 데이터 검사 시 값이 없어도 된다는 의미이다
 
@@ -30,11 +31,12 @@ class Answer(models.Model):
     ALTER TABLE `pybo_answer`     ADD CONSTRAINT `pybo_answer_question_id_e174c39f_fk_pybo_question_id`
     FOREIGN KEY (`question_id`) REFERENCES `pybo_question` (`id`);
     """
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
 
 class Comment(models.Model):
     """
