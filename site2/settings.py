221a4d7fc240c6466ab5bec6d168864a60255e97
HOME-2700 <metalhwal@gmail.com>
2021-12-27 10:56:40 +0900

    pybo: 3-05 [2] login page and error handling

--- site2/settings.py
+++ site2/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pybo.apps.PyboConfig',
-    'common.apps.PyboConfig',
+    'common.apps.CommonConfig',
 ]
 
 MIDDLEWARE = [
