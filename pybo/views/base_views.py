c5f679886b25bd0392954aa7a286577a6c17adc9
HOME-2700 <metalhwal@gmail.com>
2021-12-28 01:27:12 +0900

    pybo: 3-15 게시판 검색 기능

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
@@ -11,12 +12,23 @@ def index(request: HttpRequest) -> HttpResponse:
     """
     # input parameter
     page = request.GET.get('page', '1')  # Page
+    kw = request.GET.get('kw', '')  # search keyword
+
+    # retrieve
     question_list = Question.objects.order_by('-create_date')
-    # paging
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |              # 질문.제목 검색
+            Q(content__icontains=kw) |              # 질문.내용 검색
+            Q(author__username__icontains=kw) |     # 질문.글쓴이.username 검색
+            Q(answer__author__username__icontains=kw)  # 질문.답변.글쓴이.username 검색
+        ).distinct()
+
+    # pagination
     paginator = Paginator(question_list, 10)  # 10 articles per page
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = { 'question_list': page_obj, 'page': page, 'kw': kw }
     return render(request, 'pybo/question_list.html', context)
 
 
