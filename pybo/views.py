023ecc06e32c0f7fd25aa29ca67e7fcee28d4ced
HOME-2700 <metalhwal@gmail.com>
2021-12-27 15:30:44 +0900

    pybo: 3-09 [2] add modify button and relevant methods

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.contrib import messages
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
@@ -70,4 +71,26 @@ def question_create(request: HttpRequest) -> HttpResponse:
     else: # if method is  GET
         form = QuestionForm()
     context = { 'form': form }
+    return render(request, 'pybo/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request: HttpRequest, question_id: int) -> HttpResponse:
+    """
+    pybo 질문 수정
+    """
+    question =get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다.')
+        return redirect('pybo:detail', question_id=question.id)
+
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = { 'form': form}
     return render(request, 'pybo/question_form.html', context)
