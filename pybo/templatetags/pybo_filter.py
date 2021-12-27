c525b0813b1ec63d97f0c8d2e3e228395478f3b1
HOME-2700 <metalhwal@gmail.com>
2021-12-28 01:01:48 +0900

    pybo: 3-14 Markdown 게시글에 기능추가

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,7 +1,26 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
+
 
 @register.filter
 def sub(value, arg):
     return value - arg
+
+
+@register.filter()
+def mark(value):
+    extensions = ['nl2br',  # New line to break <br>
+                  'fenced_code',  # Fenced code blocks
+                  'codehilite',  # Code Highlight
+                  ]
+    extension_configs = {
+        'markdown.extensions.codehilite': {
+            'linenums': True,
+            'use_pygments': True,
+            'noclasses': True
+        }
+    }
+    return mark_safe(markdown.markdown(value, extensions=extensions, extension_configs=extension_configs))
